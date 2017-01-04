d8d31baa42ac4f9ddecee6192e1043dff9f86e8f
gilmardealcantara <gilmardealcantara@gmail.com>
2017-01-04 15:19:48 -0200

    Add tests request controllers.

--- test/requests/test_home_controllers.py
+++ test/requests/test_home_controllers.py
@@ -6,3 +6,11 @@ class TestHomeController(BaseTestCase):
     def test_home_path_should_be_ok(self):
         response = self.client.get("/")
         self.assert_200(response)
+
+    def test_tables_path_should_be_ok(self):
+        response = self.client.get("/tables")
+        self.assert_200(response)
+
+    def test_views_path_should_be_ok(self):
+        response = self.client.get("/views")
+        self.assert_200(response)
