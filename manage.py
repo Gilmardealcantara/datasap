5ecd3ee67fc5beef273a8df416384062f85c078a
gilmardealcantara <gilmardealcantara@gmail.com>
2017-01-06 17:16:25 -0200

    Demo

--- manage.py
+++ manage.py
@@ -3,7 +3,12 @@ from app import manager
 from scripts.load_data import LoadData
 from scripts.aggregation import AggregationData
 from flask_migrate import MigrateCommand
+import requests
 
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 manager.add_command('load', LoadData)
 manager.add_command('agg', AggregationData)
