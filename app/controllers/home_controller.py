0f60a5d2d9612709f64bdfd30ab62c17cf32ae34
gilmardealcantara <gilmardealcantara@gmail.com>
2017-01-06 15:18:00 -0200

    Update views layout.

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -6,12 +6,3 @@ blueprint = Blueprint('home_controller', __name__, url_prefix='/')
 @blueprint.route("/")
 def home():
     return render_template('home/all.html')
-
-@blueprint.route("tables")
-def table():
-    return render_template('home/table.html')
-
-@blueprint.route("views")
-def views():
-    return render_template('home/views.html')
-
--- test/requests/test_home_controllers.py
+++ test/requests/test_home_controllers.py
@@ -7,10 +7,3 @@ class TestHomeController(BaseTestCase):
         response = self.client.get("/")
         self.assert_200(response)
 
-    def test_tables_path_should_be_ok(self):
-        response = self.client.get("/tables")
-        self.assert_200(response)
-
-    def test_views_path_should_be_ok(self):
-        response = self.client.get("/views")
-        self.assert_200(response)
